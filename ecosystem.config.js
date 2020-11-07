85587907bc472f76b9e8989dcdc1d5bf910773de
Clément Vétillard <contact@clementvtrd.fr>
2020-11-07 20:04:52 +0100

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  apps : [{
-    script: 'src/bot.js',
-    watch: '.'
+  apps: [{
+    name: "bot of clans",
+    script: 'src/bot.js'
   }],
 
   deploy : {
     production : {
       user : 'root',
       host : '138.68.111.64',
-      ref  : 'origin/master',
-      repo : 'clementvtrd',
-      path : './',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      ref: 'origin/master',
+      repo: 'git@github.com:clementvtrd/bot-of-clans',
+      path: '/var/www/bot-of-clans'
     }
   }
 };
