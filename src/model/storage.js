d9053f8e77ce7a9196b6f4b221af4d56d881b1cc
Clément Vétillard <contact@clementvtrd.fr>
2020-11-08 14:42:19 +0100

    Begin storage object

--- src/model/storage.js
+++ src/model/storage.js
@@ -1,46 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb+srv://root:root@127.0.0.1?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
-
-async function linkClan(id, tag) {
-  await client.connect();
-  try {
-    const database = client.db('bot-of-clans');
-    const clans = database.collection('clans');
-    const filter = { id: id };
-    const options = { upsert: true };
-    const updateTag = {
-      $set: {
-        tag: tag
-      }
-    }
-    const result = await clans.updateOne(filter, updateTag, options);
-  } finally {
-    client.close();
-  }
-}
-
-async function linkPlayer(id, tag) {
-  await client.connect();
-  try {
-    const database = client.db('bot-of-clans');
-    const clans = database.collection('players');
-    const filter = { id: id };
-    const options = { upsert: true };
-    const updateTag = {
-      $set: {
-        tag: tag
-      }
-    }
-    const result = await clans.updateOne(filter, updateTag, options);
-  } finally {
-    client.close();
-  }
-}
-
-module.exports = {
-  linkPlayer,
-  linkClan
-}
